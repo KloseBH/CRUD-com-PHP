da915daf1180de4b1c7d405baa420a8c05aa1930
KloseBH <danielhabdias@gmail.com>
2024-09-27 09:21:08 -0300

    Controller criado

---
+++ controllers/ContactController.php
@@ -0,0 +1,34 @@
+<?php
+    require_once './models/Contact.php';
+
+    class ContactController{
+        private $contactModel;
+
+        public function __construct($connection)
+        {
+            $this->contactModel = new Contact($connection);
+        }
+
+        public function create($nome, $email, $telefone){
+            $this->contactModel->create($nome, $email, $telefone);
+            header('Location /CRUD-COM-PHP/views/index.php');
+            exit();
+        }
+
+        public function read(){
+            return $this->contactModel->read();
+        }
+
+        public function update($nome, $email, $telefone, $id){
+            $this->contactModel->update($nome, $email, $telefone, $id);
+            header('Location /CRUD-COM-PHP/views/index.php');
+            exit();
+        }
+
+        public function delete($id){
+            $this->contactModel->delete($id);
+            header('Location /CRUD-COM-PHP/views/index.php');
+            exit();
+        }
+    }
+?>
